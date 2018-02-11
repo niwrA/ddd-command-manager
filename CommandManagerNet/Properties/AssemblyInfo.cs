bc8e7c9e974797ff7add1cf2f9ecdda53d60793a
Arwin van Arum <arwin.vanarum@cgm.com>
2018-02-11 23:36:47 +0100

    Fix incorrect assemblyinfo

--- CommandManagerNet/Properties/AssemblyInfo.cs
+++ CommandManagerNet/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("CommandManagerNet")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("CompuGroup Medical SE")]
-[assembly: AssemblyProduct("CommandManagerNet")]
-[assembly: AssemblyCopyright("Copyright © CompuGroup Medical SE 2018")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CommandManager")]
+[assembly: AssemblyCopyright("2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.5.0")]
+[assembly: AssemblyFileVersion("1.0.5.0")]
