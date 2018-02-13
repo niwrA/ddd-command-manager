f6273c0bcce4093fc68b849a80415fd9c0266648
Arwin van Arum <arwin.vanarum@cgm.com>
2018-02-13 15:09:35 +0100

    Add TransactionScope to PersistChanges
    - and added code comments to document the api

--- CommandsShared/CommandManager.cs
+++ CommandsShared/CommandManager.cs
@@ -3,6 +3,7 @@ using niwrA.CommandManager.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Transactions;
 
 namespace niwrA.CommandManager
 {
@@ -10,6 +11,10 @@ namespace niwrA.CommandManager
   {
     private ICommandDtoToCommandConverter _converter;
     private ICommandService _service;
+    /// <summary>
+    /// The default constructor creates an in memory commandstate repository
+    /// and a default DateTimeProvider that uses UTC to provide timestamps
+    /// </summary>
     public CommandManager()
     {
       var repo = new CommandStateRepositoryInMemory();
@@ -17,43 +22,83 @@ namespace niwrA.CommandManager
       _converter = new CommandDtoToCommandConverter(repo, dateTimeProvider);
       _service = new CommandService(repo, dateTimeProvider);
     }
+    /// <summary>
+    /// Specify your own commandstate repository implementation and your own DateTimeProvider
+    /// implementation with this constructor
+    /// </summary>
+    /// <param name="repo"></param>
+    /// <param name="dateTimeProvider"></param>
     public CommandManager(ICommandStateRepository repo, IDateTimeProvider dateTimeProvider)
     {
       _converter = new CommandDtoToCommandConverter(repo, dateTimeProvider);
       _service = new CommandService(repo, dateTimeProvider);
     }
+    /// <summary>
+    /// Specify your own CommandDtoToCommandConverter and your own CommandService with this constructor.
+    /// This is mostly used for testing.
+    /// </summary>
+    /// <param name="converter"></param>
+    /// <param name="service"></param>
     public CommandManager(ICommandDtoToCommandConverter converter, ICommandService service)
     {
       _converter = converter;
       _service = service;
     }
+    /// <summary>
+    /// Process the specified commands (execute them)
+    /// </summary>
+    /// <param name="commands"></param>
     public void ProcessCommands(IEnumerable<ICommandDto> commands)
     {
       var typedCommands = _converter.ConvertCommands(commands);
       _service.ProcessCommands(typedCommands);
     }
+    /// <summary>
+    /// Process all imported commands. This gets all commands that are currently in the commandstaterepository
+    /// that have not yet been marked as processed (ExecutedOn is null)
+    /// </summary>
+    /// <returns></returns>
     public int ProcessImportedCommands()
     {
       var importedCommands = _converter.GetUnprocessedCommands();
       ProcessCommands(importedCommands);
       return ((ICollection<ICommandDto>)importedCommands).Count;
     }
+    /// <summary>
+    /// Merge the specified commands by applying them to a state that was rebuilt by executing all previous
+    /// commands.
+    /// </summary>
+    /// <param name="commands"></param>
     public void MergeCommands(IEnumerable<ICommandDto> commands)
     {
       var typedCommands = _converter.ConvertCommands(commands);
       _service.MergeCommands(typedCommands, _converter);
     }
+    /// <summary>
+    /// Add configurations that target specific commands, so you can route specific commands for a particular
+    /// (root) entity also to one or more additional domains.
+    /// Use AddProcessorConfigs to route all domains for a specific (root) entity to a processor (service)
+    /// </summary>
+    /// <param name="configs"></param>
     public void AddCommandConfigs(IEnumerable<ICommandConfig> configs)
     {
       _converter.AddCommandConfigs(configs);
     }
+    /// <summary>
+    /// Configure all commands for a specific entity (root) to use the specified assembly
+    /// </summary>
+    /// <param name="configs"></param>
     public void AddProcessorConfigs(IEnumerable<IProcessorConfig> configs)
     {
       _converter.AddProcessorConfigs(configs);
     }
+    /// <summary>
+    /// Call all services to persist their state. This is executed in a transactionscope, so if one fails,
+    /// no changes should be made.
+    /// </summary>
     public void PersistChanges()
     {
-      _service.PersistChanges();
+        _service.PersistChanges();
     }
   }
 }
--- CommandsShared/CommandService.cs
+++ CommandsShared/CommandService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Transactions;
 
 namespace niwrA.CommandManager
 {
@@ -17,16 +18,29 @@ namespace niwrA.CommandManager
       _repo = repo;
       _dateTimeProvider = dateTimeProvider;
     }
+    /// <summary>
+    /// Tells all services to persist all changes, including saving commands if a repository is used,
+    /// all combined in the same transactionscope, so if one fails, all fail
+    /// </summary>
     public void PersistChanges()
     {
-      // todo: wrap in transaction
-      _repo.PersistChanges();
-      // todo: persist changes on all services / repositories
-      foreach(var config in _configs.Values)
+      var processors = GetAllConfiguredProcessors();
+      using (TransactionScope scope = new TransactionScope())
       {
-      };
-
+        foreach (var processor in processors)
+        {
+          processor.PersistChanges();
+        }
+        _repo.PersistChanges();
+        scope.Complete();
+      }
     }
+    /// <summary>
+    /// Merge commands is used to rebuild the target (entity) of a command by rerunning all previous commands
+    /// first and executing the new commands against the rebuilt target (entity).
+    /// </summary>
+    /// <param name="commands"></param>
+    /// <param name="converter"></param>
     public void MergeCommands(IEnumerable<ICommand> commands, ICommandDtoToCommandConverter converter)
     {
       // todo: replay should be done once for each unique entity, or we need to refresh
@@ -46,6 +60,10 @@ namespace niwrA.CommandManager
         ProcessCommand(command);
       }
     }
+    /// <summary>
+    /// Process the specified commands (Executes them)
+    /// </summary>
+    /// <param name="commands"></param>
     public void ProcessCommands(IEnumerable<ICommand> commands)
     {
       var processed = new List<ICommand>();
@@ -54,11 +72,20 @@ namespace niwrA.CommandManager
         ProcessCommand(command);
       }
     }
+    /// <summary>
+    /// Process a single command (execute it)
+    /// </summary>
+    /// <param name="command"></param>
     public void ProcessCommand(ICommand command)
     {
       command.Execute();
       command.ExecutedOn = _dateTimeProvider.GetServerDateTime();
     }
+    /// <summary>
+    /// Create a command of the specified type.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
     public ICommand CreateCommand<T>() where T : ICommand, new()
     {
       var command = new T()
@@ -68,6 +95,17 @@ namespace niwrA.CommandManager
       command.Guid = Guid.NewGuid();
       command.CreatedOn = _dateTimeProvider.GetServerDateTime();
       return command;
+    }
+
+    /// <summary>
+    /// Gets all processors configured in the command configurations
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerable<ICommandProcessor> GetAllConfiguredProcessors()
+    {
+      var processors = _configs.Values.Select(w => w.Processor).Distinct().ToList();
+      var commandProcessors = _commandConfigs.Values.Select(w => w.Processor).Distinct().ToList();
+      return processors.Union(commandProcessors).ToList();
     }
   }
 }
