88ce20a4dabecfc9d25582e5f54a8cde84d36bad
Arwin van Arum <arwin.vanarum@cgm.com>
2018-07-24 09:45:09 +0200

    Set transactionscope timeout to 1 hour

--- CommandManagerNet/PersistAllChanges.cs
+++ CommandManagerNet/PersistAllChanges.cs
@@ -7,19 +7,20 @@ using System.Transactions;
 
 namespace niwrA.CommandManager
 {
-  public class PlatformSpecific
-  {
-    public void PersistAllChanges(IEnumerable<ICommandProcessor> processors, ICommandService service)
+    public class PlatformSpecific
     {
-      using (TransactionScope scope = new TransactionScope())
-      {
-        foreach (var processor in processors)
+        public void PersistAllChanges(IEnumerable<ICommandProcessor> processors, ICommandService service)
         {
-          processor.PersistChanges();
+            using (TransactionScope scope = new TransactionScope(TransactionScopeOption.Required,
+                                         new System.TimeSpan(0, 60, 0)))
+            {
+                foreach (var processor in processors)
+                {
+                    processor.PersistChanges();
+                }
+                service.PersistChanges();
+                scope.Complete();
+            }
         }
-        service.PersistChanges();
-        scope.Complete();
-      }
     }
-  }
 }
